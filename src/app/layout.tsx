4b691f9e3316fdf7cdd961a194a78b4b6ccb8d0f
Ega Erinovian <72180780+ega-erinovian@users.noreply.github.com>
2025-08-11 14:08:01 +0700

    feat: added login features (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import NextAuthProvider from "./providers/NextAuthProvider";
+import ReactQueryProvider from "./providers/ReactQueryProviders";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,7 +29,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <NextAuthProvider>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
